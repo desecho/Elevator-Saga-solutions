aa5fe039333aeec197413130f781ba485d957a13
Anton Samarchyan <desecho@gmail.com>
2023-11-26 11:28:39 -0500

    Changes

--- 2.js
+++ 2.js
@@ -1,32 +1,25 @@
 {
     init: function(elevators, floors) {
-        elevators.forEach(function(elevator){
-            elevator.on("floor_button_pressed", function(floorNum) {
-
-                setTimeout(function(){
+        elevators.forEach(function (elevator) {
+            elevator.on("floor_button_pressed", function (floorNum) {
+                setTimeout(function () {
                     elevator.goToFloor(floorNum);
                 }, 2000);
-
-
-            } );
+            });
         });
 
-        floors.forEach(function(floor){
-            floor.on("up_button_pressed", function() {
-
-                elevators.forEach(function(elevator){
-                    setTimeout(function(){
+        floors.forEach(function (floor) {
+            floor.on("up_button_pressed", function () {
+                elevators.forEach(function (elevator) {
+                    setTimeout(function () {
                         elevator.goToFloor(floor.floorNum());
                     }, 2000);
 
                 });
-
-
-            } );
+            });
         });
-
     },
-        update: function(dt, elevators, floors) {
-            // We normally don't need to do anything here
-        }
+    update: function(dt, elevators, floors) {
+        // We normally don't need to do anything here
+    }
 }
--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 
 Solutions are in files.
 
-* 1 - solves challenges 1, 8-9
+* 1 - solves challenges 1-4, 8-9
 * 2 - solves challenge 6
-* 3 - solves challenge 2-4
 
 http://play.elevatorsaga.com/
